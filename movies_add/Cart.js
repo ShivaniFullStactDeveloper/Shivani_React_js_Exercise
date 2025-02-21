834d14d1ac358180976673fbfe2fe8ae7ef319ab
ShivaniFullStactDeveloper <156500244+ShivaniFullStactDeveloper@users.noreply.github.com>
2025-02-21 10:04:14 +0530

    Add files via upload

---
+++ movies_add/Cart.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Cart.css';
+
+
+class Cart extends React.Component {
+    render() {
+        return (
+            <div className="cart" >
+                 {/* Click event to toggle cart */}
+               <button>Add</button>
+
+            </div>
+
+        );
+    };
+}
+export default Cart;
